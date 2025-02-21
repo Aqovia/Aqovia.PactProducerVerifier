65ef87036bba22c859d60abdb20286a09db1dbba
Venkata Dorisala <vdorisala@digitalrealty.com>
2025-02-21 15:41:13 +0000

    Update ProducerVerifierConfiguration.cs

--- Aqovia.PactProducerVerifier/ProducerVerifierConfiguration.cs
+++ Aqovia.PactProducerVerifier/ProducerVerifierConfiguration.cs
@@ -10,7 +10,7 @@ namespace Aqovia.PactProducerVerifier
     {
         public string ProviderName { get; set; }
         public string ProjectName { get; set; }
-        public string PactBrokerToken{ get; set; }
+        public string PactBrokerToken { get; set; }
         public string PactBrokerUri { get; set; }
     }
 }
